5966198c1945bcf392ff12aaf261de77c56b5b1e
Manikandan1988 <Manikandan.mca@hotmail.com>
2020-05-13 22:12:20 +0530

    first commit

---
+++ RabbitMQReceiveMsg/Program.cs
@@ -0,0 +1,81 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQReceiveMsg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            string queueName = "SampleQueue";
+            var rabbitMqConnection = factory.CreateConnection();
+            var rabbitMqChannel = rabbitMqConnection.CreateModel();
+
+            rabbitMqChannel.QueueDeclare(queue: queueName,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            rabbitMqChannel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            int messageCount = Convert.ToInt16(rabbitMqChannel.MessageCount(queueName));
+            Console.WriteLine(" Listening to the queue. This channels has {0} messages on the queue", messageCount);
+
+            var consumer = new EventingBasicConsumer(rabbitMqChannel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body.ToArray());
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Message received: " + message);
+                rabbitMqChannel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                Thread.Sleep(1000);
+            };
+            rabbitMqChannel.BasicConsume(queue: queueName,
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            Thread.Sleep(1000 * messageCount);
+            Console.WriteLine(" Connection closed, no more messages.");
+            Console.ReadLine();
+            //var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            //using (var connnection = factory.CreateConnection())
+            //{
+            //    using (var channel = connnection.CreateModel())
+            //    {
+            //        channel.QueueDeclare(queue: "SampleQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            //        var consumer = new EventingBasicConsumer(channel);
+            //        consumer.Received += (model, ea) =>
+            //        {
+            //            var body = ea.Body;
+            //            var message = Encoding.UTF8.GetString(body.ToArray());
+            //            Console.WriteLine(" [x] Received {0}", message);
+            //        };
+            //        channel.BasicConsume(queue: "SampleQueue",
+            //                             autoAck: true,
+            //                             consumer: consumer);
+            //    }
+
+            //}
+            //Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var receivedMsg = Encoding.UTF8.GetString(body.ToArray());
+            Console.WriteLine("Message Received {0}", receivedMsg);
+        }
+    }
+}
---
+++ RabbitMQSendMsg/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQSendMsg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RabbitMQ Send Message:");
+            Console.WriteLine("Do you want to sent message? Press 'Y' to contineue");
+            while (Console.ReadLine() == "Y")
+            {
+                Console.WriteLine("Please enter the message here do you want to sent:");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                string inputMessage = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connnection = factory.CreateConnection())
+                {
+                    using (var channel = connnection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: "SampleQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                        var bodyMessage = Encoding.UTF8.GetBytes(inputMessage);
+
+                        channel.BasicPublish(exchange: "", routingKey: "SampleQueue", basicProperties: null, body: bodyMessage);
+                        
+                        Console.WriteLine("Message Sent {0}", Encoding.UTF8.GetString(bodyMessage));
+                    }
+                }
+                Console.WriteLine("Do you want to sent message again? Press 'Y' to contineue");
+            }
+
+        }
+
+       
+    }
+}
